8d75efee7b04fce09ab3e6d63b3642fe80f49ac5
Scizor <scizor@MacBook-Pro-de-Matheus-2.local>
2024-05-02 03:04:07 -0300

    Creating SecondaryButton component

---
+++ Packages/DesignSystem/Sources/DesignSystem/Buttons/SecondaryButton.swift
@@ -0,0 +1,74 @@
+//
+//  SecondaryButton.swift
+//
+//
+//  Created by Scizor on 5/2/24.
+//
+
+import UIKit
+
+final public class SecondaryButton: UIButton {
+    
+    public var layout: SecondaryButton.Layout = .wide {
+        didSet { setHeight() }
+    }
+
+    public override var isEnabled: Bool {
+        didSet { setLayout() }
+    }
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLayout()
+    }
+
+    public init(layout: SecondaryButton.Layout) {
+        self.layout = layout
+        super.init(frame: .zero)
+        setLayout()
+    }
+
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        setHeight()
+    }
+
+    private func setHeight() {
+        NSLayoutConstraint.activate([self.heightAnchor.constraint(equalToConstant: layout.height)])
+    }
+
+    private func setLayout() {
+        self.backgroundColor = .BrandColors.pokeballRed
+        self.layer.cornerRadius = layout.cornerRadius
+        self.titleLabel?.font = UIFont.fontType(.button)
+        self.layer.borderWidth = CGFloat(1)
+        self.layer.borderColor = UIColor.BrandColors.pikachuYellow.cgColor
+        self.setTitleColor(self.isEnabled ? .TextColors.textPrimary : .TextColors.textDisabled, for: .normal)
+    }
+}
+
+extension SecondaryButton {
+    public enum Layout {
+        case wide
+        case thin
+
+        var cornerRadius: CGFloat {
+            switch self {
+            case .wide,
+                 .thin:
+                return CGFloat(8)
+            }
+        }
+
+        var height: CGFloat {
+            switch self {
+            case .wide: return CGFloat(48)
+            case .thin: return CGFloat(40)
+            }
+        }
+    }
+}
