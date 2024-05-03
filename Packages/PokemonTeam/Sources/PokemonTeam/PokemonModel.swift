b0d196c8e07a56fcabd3bc94847f4e638b65b286
Scizor <scizor@MacBook-Pro-de-Matheus-2.local>
2024-05-03 01:08:55 -0300

    Creating PokemonTeamRepository and PokemonModel

---
+++ Packages/PokemonTeam/Sources/PokemonTeam/PokemonModel.swift
@@ -0,0 +1,15 @@
+//
+//  PokemonModel.swift
+//
+//
+//  Created by Scizor on 5/3/24.
+//
+
+import Foundation
+
+struct PokemonModel {
+    let front: Data?
+    let id: Int
+    let name: String?
+    let types: [String]?
+}
---
+++ Packages/PokemonTeam/Sources/PokemonTeam/PokemonTeamRepository.swift
@@ -0,0 +1,14 @@
+//
+//  PokemonTeamRepository.swift
+//
+//
+//  Created by Scizor on 5/3/24.
+//
+
+import CoreData
+
+protocol PokemonTeamRepository {
+    func retrieve(completion: @escaping (Result<PokemonModel, Error>) -> Void)
+    func insert(pokemon: PokemonModel, completion: @escaping (Result<Never, Error>) -> Void)
+    func delete(id: Int, completion: @escaping (Result<Never, Error>) -> Void)
+}
