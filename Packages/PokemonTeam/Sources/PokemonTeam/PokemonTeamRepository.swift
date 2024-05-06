427bee382eef47f2e1f875a5663fb157bbda86f2
Scizor <scizor@MacBook-Pro-de-Matheus-2.local>
2024-05-06 20:45:54 -0300

    creating PokemonTeamRepositoryImplementation

--- Packages/PokemonTeam/Sources/PokemonTeam/PokemonTeamRepository.swift
+++ Packages/PokemonTeam/Sources/PokemonTeam/PokemonTeamRepository.swift
@@ -5,10 +5,38 @@
 //  Created by Scizor on 5/3/24.
 //
 
-import Foundation
+import CoreData
 
 protocol PokemonTeamRepository {
     func retrieve() async throws -> [PokemonModel]
     func insert(pokemon: PokemonModel) async throws
     func delete(id: Int) async throws
+}
+
+final class PokemonTeamRepositoryImplementation: PokemonTeamRepository {
+    private let coreDataStore: CoreDataPokemonStore
+    
+    init(coreDataStore: CoreDataPokemonStore = .init(container: NSPersistentContainer.init(name: .containerName))) {
+        self.coreDataStore = coreDataStore
+    }
+    
+    func retrieve() async throws -> [PokemonModel] {
+        return try await coreDataStore.retrieve()
+    }
+    
+    func insert(pokemon: PokemonModel) async throws {
+        let pokemons = try await retrieve()
+        guard pokemons.count < 6, pokemons.filter({ $0.id == pokemon.id }).count > 0 else { return }
+        try await coreDataStore.insert(pokemon: pokemon)
+    }
+    
+    func delete(id: Int) async throws {
+        try await coreDataStore.delete(id: id)
+    }
+}
+
+private extension String {
+    static var containerName: String {
+        return "Pokemon"
+    }
 }
