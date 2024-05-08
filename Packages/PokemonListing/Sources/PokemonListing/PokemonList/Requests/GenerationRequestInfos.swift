2df9c7b6143e5f9ba8b3bd60075f9fd6107a6bd2
TheusMartins <ml_martins@yahoo.com>
2024-05-08 01:41:51 -0300

    Creating GenerationRequestInfos

--- Packages/PokemonListing/Sources/PokemonListing/PokemonList/Requests/GenerationRequestInfos.swift
+++ Packages/PokemonListing/Sources/PokemonListing/PokemonList/Requests/GenerationRequestInfos.swift
@@ -6,3 +6,27 @@
 //
 
 import Network
+
+enum GenerationRequestInfos {
+    case getGenerations
+}
+
+extension GenerationRequestInfos: RequestInfos {
+    var endpoint: String {
+        .endpoint
+    }
+    
+    var method: Network.HTTPMethod {
+        .get
+    }
+    
+    var parameters: [String : Any]? {
+        nil
+    }
+}
+
+private extension String {
+    static var endpoint: String {
+        "generation"
+    }
+}
